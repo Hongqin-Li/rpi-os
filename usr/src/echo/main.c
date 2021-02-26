d74bf9c88fed620ba93302f9c1343903fb6d1d10
Hongqin-Li <ihongqinli@gmail.com>
2021-02-26 13:00:53 +0800

    feat: add echo user program

--- usr/src/echo/main.c
+++ usr/src/echo/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
 int
-main() {
-    int nbuf = 512;
-    char buf[nbuf];
-    char *s;
+main(int argc, char *argv[]) {
 
-    while ((s = fgets(buf, nbuf, stdin)) != NULL)
-        printf("%s", s);
+    if (argc == 1) {
+        static char buf[512];
+        char *s;
+        while ((s = fgets(buf, 512, stdin)) != NULL)
+            printf("%s", s);
+        return 0;
+    }
+
+    int nflag;
+    if (*++argv && !strcmp(*argv, "-n")) {
+        ++argv;
+        nflag = 1;
+    } else {
+        nflag = 0;
+    }
+
+    while (*argv) {
+        fputs(*argv, stdout);
+        if (*++argv)
+            putchar(' ');
+    }
+    if (!nflag)
+        putchar('\n');
 
     return 0;
 }
